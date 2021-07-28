ab66ac00971837f42ae65a4276aff9638dfd87f4
shailiran <shailiran@gmail.com>
2021-07-28 12:04:40 +0300

    finish home route

--- app/routes.py
+++ app/routes.py
@@ -3,30 +3,31 @@ from flask.globals import request
 
 from app import db
 from .models import URL
+from .utils import base62_decoder, base62_encoder
 
 short = Blueprint('short', __name__)
 
 @short.route('/', methods=['POST', 'GET'])
 def home():
   if request.method == 'POST':
-    url_received = request.form['mn']
+    url_received = request.form['nm'] # TODO - Check!!!!
     found_url = URL.query.filter_by(original_url=url_received).first()
 
     if found_url:
       return redirect(url_for('display_short_url', url=found_url.short_url))
     else:
-      # short_url = shorten_url()
-      # print(short_url)
-      # new_url = Urls(url_received, short_url)
-      # db.session.add(new_url)
-      # db.session.commit()
-      # return redirect(url_for("display_short_url", url=short_url))
-      pass
+      short_url = base62_encoder(url_received)
+      print(short_url)
+      new_url = URL(url_received, short_url)
+      db.session.add(new_url)
+      db.session.commit()
+      return redirect(url_for("display_short_url", url=short_url))
   else:
     return render_template('url_page.html')
 
 @short.route('/<short_url>')
 def redirect_to_url(short_url):
+  # original_url = base62_decoder(short_url)
   original_url = URL.query.filter_by(short_url=short_url).first()
   if original_url:
     redirect(original_url.original)
